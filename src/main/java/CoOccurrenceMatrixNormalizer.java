eef3babbb609568df4e4f8854fb5f4b2e633c0ef
scloudyy <onecloud.shen@gmail.com>
2018-08-23 23:46:21 +0800

    feat: add CoOccurrenceMatrixNormalizer main

--- src/main/java/CoOccurrenceMatrixNormalizer.java
+++ src/main/java/CoOccurrenceMatrixNormalizer.java
@@ -1,7 +1,12 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -47,5 +52,27 @@ public class CoOccurrenceMatrixNormalizer {
                 context.write(new Text(outputKey), new Text(outputValue));
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(CoOccurrenceMatrixNormalizer.class);
+
+        job.setMapperClass(MatrixNomalizerMapper.class);
+        job.setReducerClass(MatrixNomalizerRudecer.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        TextInputFormat.setInputPaths(job, new Path(args[0]));
+        TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
     }
 }
