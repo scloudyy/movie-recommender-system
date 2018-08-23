8436eb54c2dd8edc590c88faa90f71510d42fb75
scloudyy <onecloud.shen@gmail.com>
2018-08-23 22:48:10 +0800

    feat: add MatrixGeneratorReducer

--- src/main/java/CoOccurrenceMatrixGenerator.java
+++ src/main/java/CoOccurrenceMatrixGenerator.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
@@ -24,6 +25,22 @@ public class CoOccurrenceMatrixGenerator {
                     context.write(new Text(movie1 + ":" + movie2), new IntWritable(1));
                 }
             }
+        }
+    }
+
+    public static class MatrixGeneratorReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            // inputKey: movie1:movie2
+            // inputValue: <1,1,1...>
+            // outputKey: movie1:movie2
+            // outputValue: sum
+            int sum = 0;
+            for (IntWritable value : values) {
+                sum += value.get();
+            }
+
+            context.write(key, new IntWritable(sum));
         }
     }
 }
