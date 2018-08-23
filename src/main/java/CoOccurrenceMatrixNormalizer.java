f05afa019489fad70cbabc32f17d02765ff0dc04
scloudyy <onecloud.shen@gmail.com>
2018-08-23 23:40:26 +0800

    feat: add MatrixNomalizerRudecer

--- src/main/java/CoOccurrenceMatrixNormalizer.java
+++ src/main/java/CoOccurrenceMatrixNormalizer.java
@@ -1,8 +1,11 @@
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CoOccurrenceMatrixNormalizer {
 
@@ -10,9 +13,9 @@ public class CoOccurrenceMatrixNormalizer {
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             // inputKey: movie1:movie2
-            // inputValue: sum
+            // inputValue: relation
             // outputKey: movie1
-            // outputValue: movie2:sum
+            // outputValue: movie2:relation
             String[] moviesRelation = value.toString().trim().split("\t");
             String[] movies = moviesRelation[0].trim().split(":");
 
@@ -20,4 +23,29 @@ public class CoOccurrenceMatrixNormalizer {
         }
     }
 
+    public static class MatrixNomalizerRudecer extends Reducer<Text, Text, Text, Text> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            // inputKey: movie1
+            // inputValue: movie2:relation
+            // outputKey: movie2
+            // outputValue: movie1:normalizeValue
+
+            int sum = 0;
+            Map<String, Integer> map = new HashMap<>(); // key: movie2, value: relation
+            for (Text value : values) {
+                String[] movieRelation = value.toString().trim().split(":");
+                int relation = Integer.valueOf(movieRelation[1]);
+                map.put(movieRelation[0], relation);
+                sum += relation;
+            }
+
+            for (Map.Entry<String, Integer> movieRelation : map.entrySet()) {
+                String outputKey = movieRelation.getKey();
+                double normalize = (double)movieRelation.getValue() / (double)sum;
+                String outputValue = key.toString() + normalize;
+                context.write(new Text(outputKey), new Text(outputValue));
+            }
+        }
+    }
 }
