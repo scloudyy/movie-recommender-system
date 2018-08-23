459d28fc932e9aeef76d7dc596b6df5103360310
scloudyy <onecloud.shen@gmail.com>
2018-08-23 16:03:14 +0800

    feat: add rawDataDividerbyUser main

--- src/main/java/rawDataDividerbyUser.java
+++ src/main/java/rawDataDividerbyUser.java
@@ -1,9 +1,15 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
+import java.util.zip.DataFormatException;
 
 public class rawDataDividerbyUser {
     public static class rawDataDividerMapper extends Mapper<LongWritable, Text, Text, Text> {
@@ -28,5 +34,27 @@ public class rawDataDividerbyUser {
             //key = user value=movie1:rating1,movie2:rating2...
             context.write(key, new Text(stringBuilder.toString().replaceFirst(",", "")));
         }
+    }
+
+    public  static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(rawDataDividerbyUser.class);
+
+        job.setMapperClass(rawDataDividerMapper.class);
+        job.setReducerClass(rawDataDividerReducer.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        TextInputFormat.setInputPaths(job, new Path(args[0]));
+        TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
     }
 }
