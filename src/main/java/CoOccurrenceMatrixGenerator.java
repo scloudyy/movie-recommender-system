3f7197d34a9d02acced1aea9d4be253deb6163c6
scloudyy <onecloud.shen@gmail.com>
2018-08-23 22:56:59 +0800

    feat: add CoOccurrenceMatrixGenerator main

--- src/main/java/CoOccurrenceMatrixGenerator.java
+++ src/main/java/CoOccurrenceMatrixGenerator.java
@@ -1,8 +1,13 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 
@@ -42,5 +47,27 @@ public class CoOccurrenceMatrixGenerator {
 
             context.write(key, new IntWritable(sum));
         }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(CoOccurrenceMatrixGenerator.class);
+
+        job.setMapperClass(MatrixGeneratorMapper.class);
+        job.setReducerClass(MatrixGeneratorReducer.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        TextInputFormat.setInputPaths(job, new Path(args[0]));
+        TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
     }
 }
